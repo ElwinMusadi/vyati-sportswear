f31ef38d094940066b7fc7d08ee8155e7c75db9b
ElwinMusadi <elwinmusadi@gmail.com>
2021-03-04 16:37:33 +0800

    Inisialisasi project & Slicing assets

---
+++ tailwind.config.js
@@ -0,0 +1,75 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      // '2xl': '1536px',
+    },
+    container: {
+      center: 'true',
+      padding: '1rem',
+    },
+    fontFamily: {
+      'body': 'Poppins',
+    },
+    extend: {
+      colors: {
+        'black-sport': {
+          500: '#242429',
+          300: '#333333',
+        },
+        'gray-sport': {
+          500: '#53505a',
+          200: '#dbd9db',
+        },
+        gold: {
+          800: '#b68438',
+          500: '#ca9b46',
+          300: '#deb866',
+        }
+      },
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--gradient-color-stops))',
+      // },
+      backgroundImage: () => ({
+        'gradient-radial-to-tr': 'radial-gradient(115% 90% at 0% 100%, var(--tw-gradient-stops))',
+        'gradient-radial-to-tl': 'radial-gradient(115% 90% at 100% 100%, var(--tw-gradient-stops))',
+        'gradient-radial-to-br': 'radial-gradient(90% 115% at 0% 0%, var(--tw-gradient-stops))',
+        'gradient-radial-to-bl': 'radial-gradient(90% 115% at 100% 0%, var(--tw-gradient-stops))',
+      }),
+      fontSize: {
+        '5.5xl': '3.25rem',
+      }
+    },
+  },
+  variants: {
+    extend: {
+      fontWeight: ["hover", "focus"],
+    },
+  },
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '540px',
+          },
+          '@screen md': {
+            maxWidth: '720px',
+          },
+          '@screen lg': {
+            maxWidth: '960px',
+          },
+          '@screen xl': {
+            maxWidth: '1140px',
+          },
+        }
+      })
+    }
+  ],
+}
